afc5a0592eb8a7a2c7f61c041b3f6a8b5b4f04d1
maureno <maurenoammar4@gmail.com>
2026-03-27 12:42:10 +0700

    style: changing color for map card

--- app/components/beranda/MapLocation.tsx
+++ app/components/beranda/MapLocation.tsx
@@ -4,9 +4,11 @@ function MapLocation() {
 	return (
 		<section className='px-4 py-14 md:px-8 md:py-20'>
 			<div className='mx-auto w-full max-w-6xl'>
-				<h2 className='text-4xl font-semibold text-[#123A66] md:text-[42px]'>Locate US on the Map</h2>
-
-				<div className='mt-7 rounded-4xl bg-[#125ff9] p-4 text-white md:mt-8 md:p-5'>
+                <div className="flex flex-col justify-center items-center">
+					<h3 className='font-bold text-[#1A8FD2]'>Visit US</h3>
+                    <h2 className='text-4xl font-semibold text-[#123A66] md:text-[42px]'>Locate US on the Map</h2>
+                </div>
+				<div className='mt-7 rounded-4xl border border-[#cfe3f3] bg-[#eaf4fb] p-4 text-[#123A66] md:mt-8 md:p-5'>
 					<div className='grid gap-5 md:grid-cols-[1fr_0.95fr]'>
 						<div className='relative min-h-70 overflow-hidden rounded-[22px] bg-[#dce6f3] md:min-h-90'>
 							<iframe
@@ -21,8 +23,8 @@ function MapLocation() {
 
 						<div className='flex min-h-70 flex-col rounded-[22px] bg-transparent p-1 md:min-h-90'>
 							<div className='max-w-2xl'>
-								<h3 className='text-3xl font-semibold md:text-[28px]'>Headquarter</h3>
-								<p className='mt-3 text-xl leading-normal md:text-[18px]'>
+								<h3 className='text-3xl font-semibold text-[#123A66] md:text-[28px]'>Headquarter</h3>
+								<p className='mt-3 text-xl leading-normal text-[#33597f] md:text-[18px]'>
 									Menara Cakrawala L. M.H Thamrin No.9, RT.2/RW.1, KB. Sirih, Kec. Menteng, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10340
 								</p>
 							</div>
@@ -30,7 +32,7 @@ function MapLocation() {
 							<div className='mt-auto pt-8 flex justify-end items-baseline-last'>
 								<a
 									href='https://maps.app.goo.gl/XC65FNt8W5aKXHhRA'
-									className='inline-flex w-full items-center justify-center gap-3 rounded-full bg-[#BDE8FA] px-6 py-4 text-lg font-medium text-[#1A8FD2] transition-colors duration-300 hover:bg-[#a8ddf4] md:w-auto md:text-xl'
+									className='inline-flex w-full items-center justify-center gap-3 rounded-full bg-[#d9edf7] px-6 py-4 text-lg font-medium text-[#1A8FD2] transition-colors duration-300 hover:bg-[#c6e4f2] md:w-auto md:text-xl'
 								>
 									View on Google Maps
 									<FiArrowRight className='size-5' />
