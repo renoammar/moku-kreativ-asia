0e34733c843a559e0e96fa5299cfbad677717294
maureno <maurenoammar4@gmail.com>
2026-03-28 22:49:05 +0700

    feat: added mini banner to eventpage

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -1,10 +1,12 @@
 import EventServices from "../components/event-page/EventServices"
+import MiniBannerEvent from "../components/event-page/MiniBannerEvent"
 import PortofolioEvent from "../components/event-page/PortofolioEvent"
 function page() {
   return (
     <>
     <EventServices />
     <PortofolioEvent />
+    <MiniBannerEvent />
     </>
   )
 }
