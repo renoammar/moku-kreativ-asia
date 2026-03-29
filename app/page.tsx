daded19deea01fe92e720fab2726993f2e93b240
maureno <maurenoammar4@gmail.com>
2026-03-30 00:05:12 +0700

    feat: integrate news and news to the main projects

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import CompanySolution from "./components/beranda/CompanySolution";
 import PressHighlights from "./components/beranda/PressHighlights";
 import MapLocation from "./components/beranda/MapLocation";
 import ContactSection from "./components/beranda/ContactSection";
+import PortofolioHighlightPage from "./components/beranda/PortofolioHighlight";
 import Footer from "./components/UI/Footer";
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <HomeHero />
       <CompanySolution />
       <PressHighlights />
+      <PortofolioHighlightPage />
       <ContactSection />
       <MapLocation />
      
