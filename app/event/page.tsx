bdf5c450ecac2d53b10c25a28b34b9d58f21c2df
maureno <maurenoammar4@gmail.com>
2026-04-04 15:00:13 +0700

    feat: 3d design

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -1,11 +1,13 @@
 import EventServices from "../components/event-page/EventServices"
 import MiniBannerEvent from "../components/event-page/MiniBannerEvent"
 import PortofolioEvent from "../components/event-page/PortofolioEvent"
+import ThreeDimensionalDesign from "../components/event-page/ThreeDimensionalDesign"
 function page() {
   return (
     <>
     <EventServices />
     <PortofolioEvent />
+    <ThreeDimensionalDesign />
     <MiniBannerEvent />
     </>
   )
